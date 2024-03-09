11486f77c0281009e1b7acf49d442a0b330bbafa
chethan hs <chethanhs74@gmail.com>
2024-03-09 19:56:24 +0530

    adding problem2

---
+++ Assignment_2/prob2.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let a = parseInt(prompt('Enter the first value'));
+let b = parseInt(prompt('Enter the second value'));
+console.log("the sum of two number is ",a+b);
+console.log("the subtraction of two number is ",a-b);
+console.log("the division of two number is ",a/b);
+console.log("the multiplication of two number is ",a*b);
