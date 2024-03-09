7ef83605db2bb12b4c07c2dfadca67adbc6bcc51
chethan hs <chethanhs74@gmail.com>
2024-03-09 15:36:21 +0530

    completed first problem

---
+++ Assignment_2/variables.js
@@ -0,0 +1,14 @@
+let num = 125;
+let name = "chethan"
+let arr = [12,23,43,34];
+let obj = {
+    name:"chethan",
+   place:"hassan",
+      id:10
+}
+let boolean = true;
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof boolean);
