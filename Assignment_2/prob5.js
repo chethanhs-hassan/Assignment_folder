4741b9cca4d756805b767931bd3e4e4d657d5783
chethan hs <chethanhs74@gmail.com>
2024-03-09 17:16:26 +0530

    addinig prob5

---
+++ Assignment_2/prob5.js
@@ -0,0 +1,13 @@
+let arr = [1,3,2,4,7,8,9];
+let sum = 0;
+for(let i=0;i<arr.length;i++){
+    sum +=arr[i];
+}
+console.log(sum)
+let sum1=0;
+let j =0;
+while(j<arr.length){
+    sum1 +=arr[j]
+    j++
+}
+console.log(sum1);
