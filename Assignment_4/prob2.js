d67a85fac10f2ac79619a91a1f9f129b5d7ab9f7
chethan hs <chethanhs74@gmail.com>
2024-03-14 18:55:22 +0530

    adding prob2

---
+++ Assignment_4/prob2.js
@@ -0,0 +1,72 @@
+/*Q2. Range Sum Query
+
+
+
+Problem Description
+You are given an integer array A of length N.
+You are also given a 2D integer array B with dimensions M x 2, where each row denotes a [L, R] query.
+For each query, you have to find the sum of all elements from L to R indices in A (0 - indexed).
+More formally, find A[L] + A[L + 1] + A[L + 2] +... + A[R - 1] + A[R] for each query.
+
+
+
+Problem Constraints
+1 <= N, M <= 10^5
+1 <= A[i] <= 10^9
+0 <= L <= R < N
+
+
+Input Format
+The first argument is the integer array A.
+The second argument is the 2D integer array B.
+
+
+Output Format
+Return an integer array of length M where ith element is the answer for ith query in B.
+
+
+Example Input
+Input 1:
+A = [1, 2, 3, 4, 5]
+B = [[0, 3], [1, 2]]
+Input 2:
+
+A = [2, 2, 2]
+B = [[0, 0], [1, 2]]
+
+
+Example Output
+Output 1:
+[10, 5]
+Output 2:
+
+[2, 4]
+ */
+let A =[1,2,3,4,5];
+let B =[[0,3],[1,2]];
+let cumarray = [];
+for(let i =0; i<A.length; i++){
+    if(i==0){
+        cumarray.push(A[0]);
+    }
+    if(i>0){
+        cumarray.push(A[i]+cumarray[i-1]);
+    }
+}
+let output =[];
+for(let j=0; j<B.length; j++){
+    let indices = B[j];
+    let l = indices[0];
+    let r = indices[1];
+    if(l==0){
+        output.push(cumarray[r])
+    }
+    if(l>0){
+        output.push(cumarray[r]-cumarray[l-1])
+    }
+    
+    
+    
+}
+console.log(output);
+//output:[ 10, 5 ]
