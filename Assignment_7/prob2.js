c1e10fb8bfeba4d21961cdad491235f5fc59bb88
chethan hs <chethanhs74@gmail.com>
2024-03-24 13:37:50 +0530

    adding

---
+++ Assignment_7/prob2.js
@@ -0,0 +1,82 @@
+
+/*Q2. Sum of All Subarrays
+
+Problem Description
+You are given an integer array A of length N.
+You have to find the sum of all subarray sums of A.
+More formally, a subarray is defined as a contiguous part of an array which we can obtain by deleting zero or more elements from either end of the array.
+A subarray sum denotes the sum of all the elements of that subarray.
+
+Note : Be careful of integer overflow issues while calculations. Use appropriate datatypes.
+
+
+
+Problem Constraints
+1 <= N <= 10^5
+1 <= A[i] <= 10^4
+
+
+Input Format
+The first argument is the integer array A.
+
+
+Output Format
+Return a single integer denoting the sum of all subarray sums of the given array.
+
+
+Example Input
+Input 1:
+A = [1, 2, 3]
+Input 2:
+
+A = [2, 1, 3]
+
+
+Example Output
+Output 1:
+20
+Output 2:
+
+19
+
+
+Example Explanation
+Explanation 1:
+The different subarrays for the given array are: [1], [2], [3], [1, 2], [2, 3], [1, 2, 3].
+Their sums are: 1 + 2 + 3 + 3 + 5 + 6 = 20
+Explanation 2:
+
+The different subarrays for the given array are: [2], [1], [3], [2, 1], [1, 3], [2, 1, 3].
+Their sums are: 2 + 1 + 3 + 3 + 4 + 6 = 19 */
+
+let A =[2, 1, 3];
+let CA = [];
+for(let i =0; i<A.length; i++){
+    if(i==0){
+        CA.push(A[i]);
+    }
+
+    else{
+        CA.push(A[i]+CA[i-1]);
+    }
+}
+
+let calculate =0;
+for(let l=0; l<A.length; l++){
+    for(let r=l; r<A.length;r++){
+        if(l==0){
+            calculate += CA[r];
+        
+              }
+
+        else{
+            calculate+=CA[r]-CA[l-1]
+            
+        }
+    }
+
+}
+
+console.log(calculate);
+
+//output:19
